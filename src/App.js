77d14821b178ebc7da37bdb217b63ad6ecf620a7
julita <julitaspahiu@gmail.com>
2022-12-22 05:19:25 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Records from './records.json';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div>
+        <a> Style Website</a>
+        <br></br>
+        {
+          Records.map(records =>{
+            return(
+              <div key={records.email}>
+                {records.first_name}
+              </div>
+            )
+          } )
+        }
+       </div>
       </header>
     </div>
   );
